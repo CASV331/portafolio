3238c72060d1e5a86cc79affdc6f2b641df257e6
casv331 <arturosandoval688@gmail.com>
2025-01-14 11:15:42 -0700

    actualizacion de texturas

--- src/componentes/BlackHole.jsx
+++ src/componentes/BlackHole.jsx
@@ -86,12 +86,11 @@ function BlackHole() {
 
     // Luna
     const moonTexture = new THREE.TextureLoader().load("/moon.jpg");
-    const normalTexture = new THREE.TextureLoader().load("/normal.jpg");
+    // const normalTexture = new THREE.TextureLoader().load("/normal.jpg");
     const moon = new THREE.Mesh(
       new THREE.SphereGeometry(3, 32, 32),
       new THREE.MeshStandardMaterial({
         map: moonTexture,
-        normalMap: normalTexture,
       })
     );
     scene.add(moon);
@@ -105,7 +104,6 @@ function BlackHole() {
       new THREE.SphereGeometry(5, 32, 32),
       new THREE.MeshStandardMaterial({
         map: planetTexture,
-        normalMap: normalTexture,
       })
     );
     scene.add(planet);
